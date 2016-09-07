b43d5368bed17e5cd7dfe5b3a393816f7cc97f7f
theboy712 <trungtan1821993@gmail.com>
2016-09-07 09:10:09 +0700

    version 1.0

---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$cars=array("Volvo","BMW","Toyota","Honda","Mercedes","Opel","Lamborghini","");
+var_export(array_chunk($cars,2));
+
+// error echo "My house is " . $COLOR . "<br>"; 
+// error echo "My boat is " . $coLOR . "<br>";
+?>
+
+</body>
+</html>
